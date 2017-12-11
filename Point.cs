a4169513e316e54d3e54f4e5f6711daab5a36e23
Jamison Macfarland <JMacfarland@carbonite.com>
2017-12-11 09:37:51 -0500

    Temporarily changed namespaces

--- Point.cs
+++ Point.cs
@@ -1,54 +1,50 @@
 ﻿using System;
-
-namespace AdventOfCodemono
+public class Point
 {
-	public class Point
+	private int _x;
+	private int _y;
+
+	public Point (int x = 0, int y = 0)
 	{
-		private int _x;
-		private int _y;
+		this._x = x;
+		this._y = y;
+	}
 
-		public Point (int x = 0, int y = 0)
-		{
-			this._x = x;
-			this._y = y;
-		}
+	public Point clone()
+	{
+		return new Point (this._x, this._y);
+	}
 
-		public Point clone()
-		{
-			return new Point (this._x, this._y);
-		}
+	public void setPos(int x, int y)
+	{
+		this._x = x;
+		this._y = y;
+	}
 
-		public void setPos(int x, int y)
-		{
-			this._x = x;
-			this._y = y;
-		}
+	public void changeX(int value)
+	{
+		this._x += value;
+	}
 
-		public void changeX(int value)
-		{
-			this._x += value;
-		}
+	public void changeY(int value)
+	{
+		this._y += value;
+	}
 
-		public void changeY(int value)
-		{
-			this._y += value;
-		}
+	public int x()
+	{
+		return this._x;
+	}
 
-		public int x()
-		{
-			return this._x;
-		}
+	public int y()
+	{
+		return this._y;
+	}
 
-		public int y()
-		{
-			return this._y;
-		}
-
-		override public string ToString ()
-		{
-			string result = "(" + this._x + ", " + this._y + ")";
-			return result;
-		}
+	override public string ToString ()
+	{
+		string result = "(" + this._x + ", " + this._y + ")";
+		return result;
 	}
 }
 
--- adventOfCode2017.cs
+++ adventOfCode2017.cs
@@ -3,8 +3,9 @@ class adventOfCode2017
 {
     static void Main()
     {
+        Day3.solve();
         //Day5.solve();
         //Day6.solve();
-        Day7.solve();
+        //Day7.solve();
     }
 }
